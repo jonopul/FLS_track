5d7d0842b2b4fb4548928ce758799c7b7147c24b
jonop <jonop@ulaval.ca>
2025-08-25 00:30:30 -0400

    Add mobile/desktop auto-switch and cache mobile HTML (v3)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,13 @@
-const CACHE='srn-pwa-v2';
-const APP_SHELL=['./','./index.html','./manifest.json','./SRN_Parts_Database_Improved.html','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='srn-pwa-v3'; // bumped to include mobile variant
+const APP_SHELL=[
+	'./',
+	'./index.html',
+	'./manifest.json',
+	'./SRN_Parts_Database_Improved.html',
+	'./SRN_Parts_Mobile_Full.html',
+	'./icons/icon-192.png',
+	'./icons/icon-512.png'
+];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
